4f9193753edf5ec946306e51c22604b1fca666ea
Sim-JunHo <jkjk11590@gmail.com>
2021-08-24 09:59:48 +0900

    update using unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,20 @@
 from selenium import webdriver
+import unittest
 
-br = webdriver.Chrome('C:\\chromedriver\\chromedriver.exe')
-br.get('http://localhost:8000')
 
-assert 'install' in br.title
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.br = webdriver.Chrome('C:\\chromedriver\\chromedriver.exe')
+        self.br.implicitly_wait(3)
+
+    def tearDown(self):
+        self.br.quit()
+
+    def test_can_start(self):
+        self.br.get('http://localhost:8000')
+        self.assertIn('To-Do', self.br.title)
+        self.fail('Finish the test!')
+
+
+if __name__ == "__main__":
+    unittest.main(warnings='ignore')
